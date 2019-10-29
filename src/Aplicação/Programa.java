4790811b7d6076013c6404b1e8a1d59cd756cbfd
Anderson Tomaz <andersontomazseguranca@gmail.com>
2019-10-29 14:48:01 -0300

    Projeto criado

--- src/Aplicação/Programa.java
+++ src/Aplicação/Programa.java
@@ -1,25 +1,26 @@
 package Aplicação;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import Entities.Comment;
-import Entities.Post;
+import Entities.Account;
+import Entities.BusinessAccount;
+import Entities.SavingsAccount;
 
 public class Programa{
 	
-	public static void main(String[] args) throws ParseException  {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	public static void main(String[] args)   {
 		
-		Comment c1 = new Comment("Have a nice trip! ");
-		Comment c2 = new Comment("How that´s awesome");
-		//dessa forma eu faço a intanciação do meu post
-		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"), "Traveling to New Zealand", "I'm going to visit this wonderful country", 12);
-		//depois do meu post instanciado irei add os meus dois comentários
-		p1.addComment(c1);
-		p1.addComment(c2);
+		Account acc = new Account(1001, "Anderson", 0,0);
+		BusinessAccount bcc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		 
+		// upcasting // é eu pegar um objeto do tipo businessAccount e atribuir em uma varialvel do tipo account
+	
+		Account acc1 = bcc;
+		Account acc2 = new BusinessAccount(1003, "Franciele", 0,0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Ana", 0.0 , 0.01);
 		
-		System.out.println(p1);
+		// downcastins // é converter um objeto da super classe para subclasse // processo inverso..
+		
+		BusinessAccount acc4 =(BusinessAccount) acc2;
+		acc4.loan(100.0);
 	}
 }
 
